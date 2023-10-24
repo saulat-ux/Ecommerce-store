1b1e77a91b64fea9b1b07b6e0cef47d12a36c0a4
saulat-ux <saulat.zubair@gmail.com>
2023-10-24 19:14:06 +0500

    backend work

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,44 @@
 console.log("Hello World!!!");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors =require("cors");
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/userRoutes");
+const errorHandler = require("./middleware/errorMiddleware");
+
+
+const app = express()
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "myotherapp"],
+        credentials: true,
+    
+    })
+)
+
+// router
+app.use("/api/v1/users", userRoute )
+
+app.get("/", (req,res) => {
+    res.send("home page..")
+})
+
+const Port  = process.env.Port || 5000
+
+// error middleware
+app.use(errorHandler)
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(Port, () => {
+            console.log(`server is running on port ${Port}`)
+        })
+    }).catch((error) => {
+        console.log(error)
+    })
