2b6ba4989addbbed23fbe7ae4424202f18c98e56
saulat-ux <saulat.zubair@gmail.com>
2023-11-12 22:36:17 +0500

    sm

--- frontend/src/redux/features/cart/cartService.js
+++ frontend/src/redux/features/cart/cartService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-const BACKEND_URL = 'https://ecommerce-store-tasleem.vercel.app/';
+const BACKEND_URL = 'https://ecommerce-store-saulat-ux.vercel.app/';
 
 
 
