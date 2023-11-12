953c1233fed91b5d729d6d0bdaa0e7922c662729
saulat-ux <saulat.zubair@gmail.com>
2023-11-12 17:37:47 +0500

    uu

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.use(
     })
 )
 
+
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', 'https://ecommerce-store-saulat.vercel.app/');
 //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
