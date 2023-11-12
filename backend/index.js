93cae7d2fa78507a446091064eee42c78d1fc755
saulat-ux <saulat.zubair@gmail.com>
2023-11-12 17:20:58 +0500

    try pls

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended: false}));
 
 app.use(
     cors({
-        origin: [""],
+        origin: ["https://shop-saulat-ux.vercel.app"],
         credentials:true,
     
     })
