c46cde9f48218f49723b769b92d59921c455bb3e
saulat-ux <saulat.zubair@gmail.com>
2023-11-12 23:42:43 +0500

    t

--- backend/index.js
+++ backend/index.js
@@ -41,8 +41,8 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://ecommerce-store-tasleem-frontend.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE ,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Max-Age', 86400);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Max-Age', '86400');
 
     next();
 });
